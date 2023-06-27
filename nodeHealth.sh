7da98d9bacafcd30142f728e8c91cc383d19138b
Vishvanath Patil <130968991+Vishvanath-Patil@users.noreply.github.com>
2023-06-27 09:49:41 +0530

    Update nodeHealth.sh

--- nodeHealth.sh
+++ nodeHealth.sh
@@ -1,3 +1,11 @@
+#####################################
+# Author : Vishvanath Patil
+# Date : 27-06-2023
+# This script monitor the system Health 
+# version : v1
+######################################
+
+
 set -x #debug mode
 set -e # exit the sript if there is an error 
 set -o pipefail
